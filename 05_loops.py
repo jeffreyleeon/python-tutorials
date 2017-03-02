3649c9d997218fc92c86497dde42d76314aa4ed7
Lee Chun On <jeffreyleeon@gmail.com>
2017-03-03 00:52:15 +0800

    Add while loop demo

---
+++ 05_loops.py
@@ -0,0 +1,10 @@
+'''
+Introducing 'while' and 'for' loops,
+Demonstrating how each of them determine end state
+Demonstrating how 'contiune' and 'break' affect the loop
+'''
+
+a = 0
+while a < 5:
+	print('a is ', a)
+	a = a + 1
